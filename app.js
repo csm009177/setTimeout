a53cb7b356d51a5dbf2691ab746adbb8064d1ac4
csm <csm009177@gamil.com>
2023-10-06 10:41:13 +0900

    settimeout test

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hello')
+setTimeout(function(){
+  console.log('first');
+}, 3000)
+setTimeout(function(){
+  console.log('second');
+}, 2000)
+setTimeout(function(){
+  console.log('third');
+}, 1000)
+// 1s === 1000
