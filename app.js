6fac2db95b43d9f11093eb4bf0bddcebffb47234
csm <csm009177@gamil.com>
2023-10-06 11:31:48 +0900

    재기함수와 지원해주는 인터벌

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-setTimeout(function(){
-  console.log('first');
-}, 3000)
-setTimeout(function(){
-  console.log('second');
-}, 2000)
-setTimeout(function(){
-  console.log('third');
-}, 1000)
-// 1s === 1000
+// setTimeout(function(){
+//   console.log('first');
+// }, 3000)
+
+// setTimeout(function(){
+//   console.log('second');
+// }, 2000)
+
+// setTimeout(function(){
+//   console.log('third');
+// }, 1000)
+// // 1s === 1000
+
+// function sosa() {
+//   setTimeout(function() {
+//     console.log('소사');
+//     sosa();
+//   }, 1000);
+// };
+
+// sosa();
+
+setInterval(function() { } , 1000);
+setInterval(function() {
+  console.log("소사 이따 향마루 ㄱ?")
+}, 1000);
